2bdf96b42aec380da7898cabc8289ee7f6e61f0f
Dev-Nandgavkar <devnandgavkar@gmail.com>
2024-10-27 21:32:33 +0530

    New Pro SetUp

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+const mainnav = document.querySelector('.header');
+
+window.addEventListener('scroll', ()=>{
+    if(document.documentElement.scrollTop > 2){
+        mainnav.classList.add('sticky');
+    } else{
+        mainnav.classList.remove('sticky');
+    }
+})
+
+// swiper
+
